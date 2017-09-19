e5d59dcf70c022ac3a89bce6dd7b3eb0a4ebe218
Tyler <supersnapple@gmail.com>
2017-09-19 06:09:05 -0500

    Upgrading to netcoreapp2.0

---
+++ codeExamples/Lambdas/SimpleLambda.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace codeExamples.Lambdas
+{
+    public class SimpleLambda
+    {
+        public void Example()
+        {
+            //Action lamda = () => Console.WriteLine("Hello OKC#!");
+            var callLamda = new CallLamda();
+            callLamda.Call(() => Console.WriteLine("Hello OKC#!"));
+            string fromOutsideTheScopeOfTheLamda = "Hello from a different scope";
+            callLamda.Call(() => Console.WriteLine(fromOutsideTheScopeOfTheLamda));
+            //int[] a = {1};
+            //var c = 1;
+            //var b = a.Select(x => x == c);
+        }
+    }
+
+    public class CallLamda
+    {
+        public void Call(Action lamda)
+        {
+            lamda();
+        }
+    }
+}
