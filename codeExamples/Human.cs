a5bda22b46b7942b382d4876b200806e267b66a6
Tyler Palesano <tpalesano@interworks.com>
2017-09-19 06:09:05 -0500

    Upgrading to netcoreapp2.0

--- codeExamples/Human.cs
+++
@@ -1,9 +0,0 @@
-namespace example3
-{
-    public class Human
-    {
-        public int INV { get; protected set; }
-        public int SPEED { get; protected set; }
-        public int MRD { get; protected set; }
-    }
-}
